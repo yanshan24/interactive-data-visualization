8b91c0dfa77672f3a3715d2ae5db26954df52a9a
yanshan24 <yanshan@ualberta.ca>
2023-03-19 12:16:20 -0600

    modify Daily Cyclist Counts by Timestamp

--- src/importData.js
+++ src/importData.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
 import Papa from 'papaparse';
+import groupBy from 'lodash/groupBy';
 import PedestriansAndBikeCountsData from './Daily_Pedestrian_and_Bike_Counts.csv';
 
 function ImportData() {
@@ -15,32 +16,42 @@ function ImportData() {
             complete: ((result) => {
                 console.log(result.data);
                 setData(result.data);
-                console.log(result.meta.fields); // add this line
+                console.log(result.meta.fields);
             })
         })
     }
-    fetchParseData()
-    }, [])
+  fetchParseData()
+  }, [])
 
-  console.log(data);
-    
+  const groupByTimestamp = groupBy(data, 'Log_Timstamp'); // group by timestamp
+
+  const traces = Object.keys(groupByTimestamp)
+  .sort((a, b) => new Date(a) - new Date(b)) // sort timestamps in ascending order
+  .map((timestamp) => {
+    const counts = groupByTimestamp[timestamp];
+    return {
+      x: counts.map((count) => count.Log_Timstamp),
+      y: counts.map((count) => count.Total_Cyclist_Count),
+      type: 'scatter',
+      mode: 'lines',
+      marker: { color: 'red' },
+      name: timestamp,
+    };
+  });
+
   return (
     <div>
       <Plot
-        data={[
-          {
-            x: data.map((d) => d.Log_Timstamp),
-            y: data.map((d) => d.Total_Cyclist_Count),
-            type: 'scatter',
-            mode: 'lines',
-            marker: { color: 'red' },
-          },
-        ]}
-        layout={{ width: 800, height: 500 }}
-        xaxis={{ title: "Date" }}
-        yaxis={{ title: "Count" }}
-      />
-      {console.log('x:', data.map((d) => d.Log_Timstamp))}
+        data={traces}
+        layout={{
+          title: 'Daily Cyclist Counts by Timestamp',
+          xaxis: { title: 'Timestamp' },
+          yaxis: { title: 'Total Cyclist Count' },
+          width: '100%',
+          height: '100%',
+          autosize: true
+        }}
+      />      
     </div>
   );
 }
