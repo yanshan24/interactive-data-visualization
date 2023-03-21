e75a4dc3a70758ca08f2f7d375a8e9c2550961b1
Abigail Xia <90875587+xyfishere@users.noreply.github.com>
2023-03-20 18:57:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import DailyCyclistsCounts from './dailyCyclistsCounts';
 import DailyPedestriansCounts from './dailyPedestriansCounts';
